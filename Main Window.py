ea35a8d79516a38ed2b824330bd93ac26abf80a8
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-07-16 10:41:32 +0800

    Made configuration on windows

--- Main Window.py
+++ Main Window.py
@@ -3,6 +3,10 @@ import tkinter as tk
 class COVIDTracker(tk.Tk):
     def __init__(self):
         super().__init__()
+    
+        #Main Window
+        self.title("COVID-19 Tracker")
+        self.geometry("1000x1000")
 
 if __name__ == "__main__":
     CTracker = COVIDTracker()
