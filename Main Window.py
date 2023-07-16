4ad196b30b19675a16a8dd4a4de5f80b67b85e04
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-07-16 10:37:30 +0800

    Encountered problems, will repeat

--- Main Window.py
+++ Main Window.py
@@ -1,10 +1,3 @@
-import tkinter as tk
+from tkinter import Tk
 
-class COVIDTracker(tk.Tk):
-    def Main(self):
-        self.title("COVIDTracker")
-        self.geometry("1000x1000")
-        self.mainloop()
-
-Ct = COVIDTracker()
-Ct.Main()
+class COVIDTracker:
