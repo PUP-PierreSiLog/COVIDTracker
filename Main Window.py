0a2c7d4b0f3b974ad169e4b2ac8e7e622b437ab5
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-07-16 10:52:44 +0800

    Added font size preferences on the title inside window

--- Main Window.py
+++ Main Window.py
@@ -10,11 +10,11 @@ class COVIDTracker(tk.Tk):
         self.geometry("500x500")
 
         #Label
-        self.HDF_title = ttk.Label(self, text = "Health Declaration Form")
+        self.HDF_title = ttk.Label(self, text = "Health Declaration Form", font="arial 12")
         self.HDF_title.pack()
 
         #Label2
-        self.HDF_Content = ttk.Label(self, text = "I declare under oath that I personally accomplished this Health Declaration form. Further, I declare that the information given are true, correct, and complete statements pursuant to the provisions of pertinent laws, rules, and regulations of the Republic of the Philippines.")
+        self.HDF_Content = ttk.Label(self, text = "I declare under oath that I personally accomplished this Health Declaration form. Further, I declare that the information given are true, correct, and complete statements pursuant to the provisions of pertinent laws, rules, and regulations of the Republic of the Philippines.", wraplength=300, justify="center")
         self.HDF_Content.pack()
 
 if __name__ == "__main__":
