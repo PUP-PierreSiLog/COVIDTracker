d1f38639806f453787bafc74e34e59514d4f8841
Pierre Zurbito <pierrevictortzurbito@iskolarngbayan.pup.edu.ph>
2023-07-20 08:35:02 +0800

    Added search results box

--- Window_3.py
+++ Window_3.py
@@ -14,6 +14,15 @@ class Admin(tk.Toplevel):
 
     def perform_search(self):
         query = self.search_entry.get()
+        results = self.search_for_name(query)
+        self.display_results(results)
+
+    def display_results(self, results):
+        self.results_listbox.delete(0, tk.END)
+
+        for result in results:
+            result_string = ",".join(result)
+            self.results_listbox.insert(tk.END, result_string)
 
     #Tkinter window
     def __init__(self):
@@ -31,7 +40,11 @@ class Admin(tk.Toplevel):
         #Search Text and Entries
         self.enter_label=tk.Label(self, text="Search by name: ")
         self.enter_label.grid(row=1, column=0)
-        self.search_entry=tk.Entry(self, width=50)
+        self.search_entry=tk.Entry(self, width=50, command=self.perform_search)
         self.search_entry.grid(row=1, column=1)
         self.search_button=tk.Button(self, text="Search")
         self.search_button.grid(row=1, column=2)
+
+        #Create results box below the field
+        results_listbox=tk.Listbox()
+        results_listbox.grid(row=2, column=0, columnspan=2)
